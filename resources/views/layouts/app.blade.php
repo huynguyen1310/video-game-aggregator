05ad75d539fd2061a7883cc767abf268e46affc0
Huy Nguyen <huy.nguyen@gumiviet.com>
2021-09-30 13:07:28 +0700

    feat: index

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Video Game Aggregator</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body class="bg-gray-900 text-white">
+<header class="border-b border-gray-800">
+    <nav class="container mx-auto flex items-center justify-between px-4 py-6">
+        <div class="flex items-center">
+            <a href="/">
+                <img src="laracasts-logo.svg" alt="" class="w-32 flex-none">
+            </a>
+            <ul class="flex ml-16 space-x-8">
+                <li><a href="#" class="hover:text-gray-400">Games</a></li>
+                <li><a href="#" class="hover:text-gray-400">Reviews</a></li>
+                <li><a href="#" class="hover:text-gray-400">Coming Soon</a></li>
+            </ul>
+        </div>
+        <div class="flex items-center">
+            <div class="relative">
+                <input type="text"
+                       class="bg-gray-800 text-sm rounded-full w-64 focus:outline-none focus:shadow-outline px-3 pl-8 py-1"
+                       placeholder="Search">
+                <div class="absolute top-0 flex items-center h-full ml-2">
+                    <svg class="fill-current text-gray-400 w-4" viewBox="0 0 24 24">
+                        <path class="heroicon-ui"
+                              d="M16.32 14.9l5.39 5.4a1 1 0 01-1.42 1.4l-5.38-5.38a8 8 0 111.41-1.41zM10 16a6 6 0 100-12 6 6 0 000 12z"/>
+                    </svg>
+                </div>
+            </div>
+            <div class="ml-6">
+                <a href="#">
+                    <img src="avatar.jpg" alt="avatar" class="rounded-full w-8">
+                </a>
+            </div>
+        </div>
+    </nav>
+</header>
+
+<main class="py-8">
+    @yield('content')
+</main>
+
+<footer class="border-t border-gray-800">
+    <div class="container mx-auto px-4 py-6">
+        Power By <a href="#" class="underline hover:text-gray-400">IGDB API</a>
+    </div>
+</footer>
+</body>
+</html>
